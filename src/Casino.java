15c91bfdb8507b9af1c4960caa4b786281c94144
maxtsaiyoung <max_tsai-young26@milton.edu>
2024-06-03 08:40:29 -0400

    Final Commit

--- src/Casino.java
+++ src/Casino.java
@@ -4,7 +4,11 @@ public class Casino {
     public Card[] Deck;
     public int counter;
     public Card parkingLot;
-    public Card firstCard = ((int)Math.random())*52;
+    public Player Dealer;
+    public int randInt;
+    public Player[] players;
+    public int dealCounter =2;
+
 
     public static void main(String[] args) {
         Casino vegas = new Casino();
@@ -17,7 +21,10 @@ public class Casino {
         //aceHearts.printInfo();
 
 
+        Dealer = new Player(0);
+
         Deck = new Card[52];
+        players = new Player[3];
         for (int k = 0; k < 4; k++) {//outer loop changes suitNum
             for (int i = 1; i < 14; i++) {//inner loop changes cardNum
                 Deck[counter] = new Card(i, k);
@@ -25,16 +32,31 @@ public class Casino {
             }
         }
         System.out.println("our og deck is below");
-        for (int x = 0; x < 51; x++) {
+        for (int x = 0; x < 52; x++) {
             Deck[x].printInfo();
 
         }
+        System.out.println("Our New Deck Is Below");
 
-        System.out.println("our new deck is below");
         shuffle();
+
         for (int x = 0; x < 52; x++) {
             Deck[x].printInfo();
         }
+
+
+        for(int z=0;z<3;z++){
+            players[z] = new Player(z+1);
+        }
+        deal();
+        Dealer.printInfo();
+        players[2].printInfo();
+
+
+        for(int i=0;i<3;i++){
+            players[i].printInfo();
+        }
+
 
     }
     public void shuffle(){
@@ -43,12 +65,27 @@ public class Casino {
         parkingLot = Deck[0];
         Deck[0]= Deck[1];
         Deck[1] = parkingLot;
-        //how i can do this 52 times
         for(int x = 0; x < 52; x++){
-            parkingLot = Deck[firstCard];
-            Deck[firstCard]= Deck[x];
-            Deck[firstCard] = parkingLot;
-
+            randInt = (int)(Math.random()*(52));
+            parkingLot = Deck[x];
+            Deck[x]= Deck[randInt];
+            Deck[randInt] = parkingLot;
         }
     }
+
+    public void deal(){
+        //deal two cards to the dealer
+        Dealer.hand[0] = Deck[0];
+        Dealer.hand[1] = Deck[1];
+
+
+        for(int x=0;x<players.length;x++){
+            for(int y=0;y<players[x].hand.length;y++){
+                players[x].hand[y] = Deck[dealCounter];
+                dealCounter = dealCounter+1;
+            }
+        }
+    }
+
+
 }
---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    public int playerNum;
+    public String playerName;
+    public int handSize;
+    public Card[] hand;
+
+public Player(int pPlayerNum){
+    playerNum = pPlayerNum;
+
+
+    hand = new Card[2];
+
+}
+public void printInfo(){
+    System.out.println("Player Number " + playerNum);
+
+    for(int i=0;i<hand.length;i++){
+        hand[i].printInfo();
+    }
+}
+
+}
